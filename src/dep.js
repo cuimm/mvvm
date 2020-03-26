5f484b61572f52f4748d79c8b20e9b5384e7e62a
mengmeng19 <mengmeng19@staff.weibo.com>
2020-03-27 01:48:49 +0800

    优化dep

--- src/dep.js
+++ src/dep.js
@@ -4,7 +4,10 @@ class Dep {
     this.subs = []
   }
   addSub(watcher) {
-    this.subs.push(watcher)
+    // 判断如果添加了watcher 就不再加入
+    if (!this.subs.find(item => item === watcher)) {
+      this.subs.push(watcher)
+    }
   }
   notify() {
     this.subs.forEach(watcher => {
